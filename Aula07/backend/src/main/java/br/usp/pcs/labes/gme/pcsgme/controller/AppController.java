3aedd1b71131bbfde6a49225736984580f728016
Fabio Levy Siqueira <levy.siqueira@gmail.com>
2024-10-31 15:58:42 -0300

    Outra solução para o problema do Spring e ReactRouter

--- Aula07/backend/src/main/java/br/usp/pcs/labes/gme/pcsgme/controller/AppController.java
+++ Aula07/backend/src/main/java/br/usp/pcs/labes/gme/pcsgme/controller/AppController.java
@@ -1,13 +1,13 @@
 package br.usp.pcs.labes.gme.pcsgme.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class AppController {
-    @GetMapping(value = {"/", ""})
+    @RequestMapping(value = { "/", "/{x:[\\w\\-]+}", "/{x:^(?!api$).*$}/*/{y:[\\w\\-]+}","/error"  })
     public String getIndex(HttpServletRequest request) {
         return "/index.html";
     }
