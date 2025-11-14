6c328a0f42bc4ab3cfa4e48aaf77eee5b441aa6c
fabio_siqueira <levy.siqueira@usp.br>
2025-11-14 17:57:39 -0300

    Evitar duplicação no DataLoader

--- Aula08/backend/src/main/java/br/usp/pcs/labes/gme/pcsgme/DataLoader.java
+++ Aula08/backend/src/main/java/br/usp/pcs/labes/gme/pcsgme/DataLoader.java
@@ -18,58 +18,64 @@ import br.usp.pcs.labes.gme.pcsgme.repository.GerenciadorDeUsuarios;
     @Bean
     ApplicationRunner carregaMateriais(GerenciadorDeMateriais materiais) {
     return args-> {
-      materiais.save(new Material("papel A4", 15));
-      materiais.save(new Material("caneta azul", 6));
-      materiais.save(new Material("lapis", 3));
-      materiais.save(new Material("borracha", 4));
+      if (materiais.count() == 0) {
+        materiais.save(new Material("papel A4", 15));
+        materiais.save(new Material("caneta azul", 6));
+        materiais.save(new Material("lapis", 3));
+        materiais.save(new Material("borracha", 4));
+      }
     };
   }
 
     @Bean
     ApplicationRunner carregaPessoas(GerenciadorDeCarros carros, GerenciadorDePessoas pessoas) {
     return args-> {
-      Pessoa p1 = new Pessoa("Fabio");
-      Pessoa p2 = new Pessoa("Ana");
-      Pessoa p3 = new Pessoa("Joao");
+        if (pessoas.count() == 0) {
+        Pessoa p1 = new Pessoa("Fabio");
+        Pessoa p2 = new Pessoa("Ana");
+        Pessoa p3 = new Pessoa("Joao");
 
-      p1 = pessoas.save(p1);
-      p2 = pessoas.save(p2);
-      p3 = pessoas.save(p3);
+        p1 = pessoas.save(p1);
+        p2 = pessoas.save(p2);
+        p3 = pessoas.save(p3);
 
-      Carro c1 = new Carro("ABC", p1);
-      Carro c2 = new Carro("ABD", p1);
-      p1.add(c1);
-      p1.add(c2);
-      Carro c3 = new Carro("XYZ", p2);
-      p2.add(c3);
+        Carro c1 = new Carro("ABC", p1);
+        Carro c2 = new Carro("ABD", p1);
+        p1.add(c1);
+        p1.add(c2);
+        Carro c3 = new Carro("XYZ", p2);
+        p2.add(c3);
 
-      // carros.save(c1);
-      // carros.save(c2);
-      // carros.save(c3);
+        // carros.save(c1);
+        // carros.save(c2);
+        // carros.save(c3);
 
-      pessoas.save(p1);
-      pessoas.save(p2);
-      pessoas.save(p3);
+        pessoas.save(p1);
+        pessoas.save(p2);
+        pessoas.save(p3);
+      }
     };
   }
 
   @Bean
   ApplicationRunner carregaUsuarios(GerenciadorDeUsuarios usuarios) {
     return args-> {
-      Usuario u1 = new Usuario("Fabio");
-      Usuario u2 = new Usuario("Ana");
-      Usuario u3 = new Usuario("Joao");
+      if (usuarios.count() == 0) {
+        Usuario u1 = new Usuario("Fabio");
+        Usuario u2 = new Usuario("Ana");
+        Usuario u3 = new Usuario("Joao");
 
-      u1 = usuarios.save(u1);
-      usuarios.save(u2);
-      usuarios.save(u3);
+        u1 = usuarios.save(u1);
+        usuarios.save(u2);
+        usuarios.save(u3);
 
-      Endereco e1 = new Endereco("Rua A", "10", "", "Cidade X", "Estado Y", "12345-678");
-      Endereco e2 = new Endereco("Rua B", "12", "apto 123", "Cidade Y", "Estado Z", "23456-789");
-      u1.add(e1);
-      u1.add(e2);
+        Endereco e1 = new Endereco("Rua A", "10", "", "Cidade X", "Estado Y", "12345-678");
+        Endereco e2 = new Endereco("Rua B", "12", "apto 123", "Cidade Y", "Estado Z", "23456-789");
+        u1.add(e1);
+        u1.add(e2);
 
-      usuarios.save(u1);
+        usuarios.save(u1);
+      }
     };
   }
 }
